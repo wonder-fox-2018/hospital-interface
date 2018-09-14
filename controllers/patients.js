952bc9bfb513168226626c43d8cb39e507a9198f
reshalem <aliisetia@gmail.com>
2018-09-14 19:43:51 +0700

    Slight Revision

--- controllers/patients.js
+++ controllers/patients.js
@@ -1,11 +1,1 @@
-var Patient = require('../models/patient.js');
-
-class PatientModel {
-    constructor(id, name, diagnosis) {
-        this.id = id;
-        this.name = name;
-        this.diagnosis = diagnosis;
-    }
-}
-
-module.exports = PatientModel;
+// Unused.
